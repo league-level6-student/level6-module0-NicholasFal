63f9d31df28911d53d3b18096c708405d14d85e4
root <root@fbd3a269bc5b>
2024-01-14 20:56:40 +0000

    I worked on mock testing

--- src/test/java/_08_mocking/models/DeliveryDriverTest.java
+++ src/test/java/_08_mocking/models/DeliveryDriverTest.java
@@ -1,48 +1,63 @@
 package _08_mocking.models;
 
-import _07_intro_to_mocking.models.Car;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.*;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import _07_intro_to_mocking.models.Car;
 
 class DeliveryDriverTest {
 
     DeliveryDriver deliveryDriver;
-
-    @BeforeEach
+    String name;
+    @Mock
+    Car car;
+    @Mock
+    CellPhone cellPhone;
+    
     void setUp() {
-
+    	MockitoAnnotations.openMocks(this);
+    	deliveryDriver = new DeliveryDriver(name, car, cellPhone);
     }
 
     @Test
     void itShouldWasteTime() {
         //given
+    	boolean expectedTimeWaste = true;
+        when(deliveryDriver.wasteTime()).thenReturn(true);
 
         //when
-
+    	boolean actualTimeWaste = deliveryDriver.wasteTime();
         //then
+    	assertEquals(expectedTimeWaste, actualTimeWaste);
     }
 
     @Test
     void itShouldRefuel() {
         //given
-
+    	boolean expectedRefuel = true;
+    	int octaneGrade = 9;
         //when
-
-        //then
+    	when(deliveryDriver.refuel(octaneGrade)).thenReturn(true);
+        boolean actualRefuel = deliveryDriver.refuel(octaneGrade);
+    	//then
+        assertEquals(expectedRefuel, actualRefuel);
+    	
     }
 
     @Test
     void itShouldContactCustomer() {
         //given
-
+    	boolean expectedContact = true;
+    	String phoneNumber = "8587275939";
         //when
-
+    	when(deliveryDriver.contactCustomer(phoneNumber)).thenReturn(true);
+    	boolean actualContact = deliveryDriver.contactCustomer(phoneNumber);
         //then
+    	assertEquals(expectedContact, actualContact);
     }
 
 }
